0ddf45e387da424cd661214b121b81753c11da9b
mft <1924257823@qq.com>
2023-07-16 14:46:58 +0800

    base.py新增构建decloud数据集的dataset函数

--- ultralytics/yolo/data/base.py
+++ ultralytics/yolo/data/base.py
@@ -284,3 +284,85 @@ class BaseDataset(Dataset):
             )
         """
         raise NotImplementedError
+
+class PairLoader(Dataset):
+    def __init__(self, data_dir, sub_dir, mode, size=512, edge_decay=0, only_h_flip=False):
+        assert mode in ['train', 'valid']
+        import random
+        random.seed(1234)
+        self.mode = mode
+        self.size = size
+        self.edge_decay = edge_decay
+        self.only_h_flip = only_h_flip
+        self.root_dir = os.path.join(data_dir, sub_dir)
+        self.img_names = sorted(os.listdir(os.path.join(self.root_dir, 'opt_cloudy')))
+        self.img_names = self.img_names[:int(len(self.img_names)*0.8)] if mode == "train" else self.img_names[int(len(self.img_names)*0.8):]
+        self.img_num = len(self.img_names)
+    def __len__(self):
+        return self.img_num
+    
+    def __getitem__(self, idx):
+        cv2.setNumThreads(0)
+        cv2.ocl.setUseOpenCL(False)
+        img_name = self.img_names[idx]
+        source_img = read_img(os.path.join(self.root_dir, 'opt_cloudy', img_name)) * 2 - 1
+        target_img = read_img(os.path.join(self.root_dir, 'opt_clear', img_name)) * 2 - 1
+        vh = source_img = read_img(os.path.join(self.root_dir, 'SAR/VH', img_name.replace("S2","S1"))) * 2 - 1
+        vv = source_img = read_img(os.path.join(self.root_dir, 'SAR/VV', img_name.replace("S2","S1"))) * 2 - 1
+        source_img = np.concatenate((source_img, np.expand_dims(vh[:,:,0],axis=2), np.expand_dims(vv[:,:,0],axis=2)), axis=2)
+
+        if self.mode == 'train':
+            [source_img, target_img] = augment([source_img, target_img], self.size, self.edge_decay, self.only_h_flip)
+        if self.mode == 'valid':
+            [source_img, target_img] = align([source_img, target_img], self.size)
+        return {'source': hwc_to_chw(source_img), 'target': hwc_to_chw(target_img), 'filename': img_name}
+
+def augment(imgs=[], size=256, edge_decay=0., only_h_flip=False):
+	H, W, _ = imgs[0].shape
+	Hc, Wc = [size, size]
+
+	# simple re-weight for the edge
+	if random.random() < Hc / H * edge_decay:
+		Hs = 0 if random.randint(0, 1) == 0 else H - Hc
+	else:
+		Hs = random.randint(0, H-Hc)
+
+	if random.random() < Wc / W * edge_decay:
+		Ws = 0 if random.randint(0, 1) == 0 else W - Wc
+	else:
+		Ws = random.randint(0, W-Wc)
+
+	for i in range(len(imgs)):
+		imgs[i] = imgs[i][Hs:(Hs+Hc), Ws:(Ws+Wc), :]
+
+	# horizontal flip
+	if random.randint(0, 1) == 1:
+		for i in range(len(imgs)):
+			imgs[i] = np.flip(imgs[i], axis=1)
+
+	if not only_h_flip:
+		# bad data augmentations for outdoor
+		rot_deg = random.randint(0, 3)
+		for i in range(len(imgs)):
+			imgs[i] = np.rot90(imgs[i], rot_deg, (0, 1))
+			
+	return imgs
+
+
+def align(imgs=[], size=256):
+	H, W, _ = imgs[0].shape
+	Hc, Wc = [size, size]
+
+	Hs = (H - Hc) // 2
+	Ws = (W - Wc) // 2
+	for i in range(len(imgs)):
+		imgs[i] = imgs[i][Hs:(Hs+Hc), Ws:(Ws+Wc), :]
+
+	return imgs
+
+def read_img(filename):
+    img = cv2.imread(filename)
+    return img[:, :, ::-1].astype('float32') / 255.0
+
+def hwc_to_chw(img):
+	return np.transpose(img, axes=[2, 0, 1]).copy()
