b055f122829529ec7b3edff76c9ecca8f5f8e627
mft <1924257823@qq.com>
2023-07-16 14:46:31 +0800

    引用decloud数据集的dataset

--- ultralytics/yolo/data/__init__.py
+++ ultralytics/yolo/data/__init__.py
@@ -1,6 +1,6 @@
 # Ultralytics YOLO 🚀, AGPL-3.0 license
 
-from .base import BaseDataset
+from .base import BaseDataset, PairLoader
 from .build import build_dataloader, build_yolo_dataset, load_inference_source
 from .dataset import ClassificationDataset, SemanticDataset, YOLODataset
 from .dataset_wrappers import MixAndRectDataset
